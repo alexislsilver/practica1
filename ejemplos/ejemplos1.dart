32bbb0aec5d74e955f2215bf49c23dfd7b93bcdf
alexis <lalexis.covarrubiasl@gmail.com>
2021-01-26 19:44:59 -0600

    Pimer Commit

---
+++ ejemplos/ejemplos1.dart
@@ -0,0 +1,10 @@
+//define una funcion
+imprimeEntero(int unNumero) {
+  print('El numero es: $unNumero'); //Imprimer en la consola
+}
+
+//Metodo principal
+main() {
+  var numero = 10; //Declaramos e inicializamos la variable
+  imprimeEntero(numero); //llama a la función
+}
